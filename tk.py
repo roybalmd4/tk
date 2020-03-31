26b0cc7e1c7df7383bc4fd38dd7cc222f56a4073
Mike Roybal <mike.roybal@usda.gov>
2020-03-30 19:26:24 -0700

    Adding tk.py

---
+++ tk.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import filedialog
+
+
+def main():
+
+    file = filedialog.askopenfilename(filetypes = (("Text files","*.txt"),("all files","*.*")))
+
+    window = Tk()
+
+    window.title("Welcome to LikeGeeks app")
+    window.geometry("400x300")
+
+    lbl = Label(window, text=file, font=("arial bold", 50))
+
+    lbl.grid(column=200, row=200)
+
+
+    window.mainloop()
+
+
+
+if __name__ == '__main__':
+    main()
