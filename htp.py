e5aca5367125ebfcb9af10faca822a9dcdc2b063
roybalmd4 <58150053+roybalmd4@users.noreply.github.com>
2020-04-02 16:13:11 -0700

    Added df2

--- htp.py
+++ htp.py
@@ -3,6 +3,14 @@ import pandas as pd
 def getData():
 
     # Reading the data from the AZMET website.
-    df1 = pd.read_csv("azmet.txt")
+    df1 = pd.read_csv("https://cals.arizona.edu/AZMET/data/0620rd.txt")
     print(df1)
 
+    # df2 = df1.filter(df1.index[1])
+    # print(df2)
+
+def main():
+    getData()
+
+if __name__ == '__main__':
+    main()
